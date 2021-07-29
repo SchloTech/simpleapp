97cc64090c532ae4e4573a4da53e20b42ae47ac8
Jake Schlottag <jschlottag.1@gmail.com>
2021-07-29 15:10:18 -0400

    first commit of simpleapp to test s2i

---
+++ index.php
@@ -0,0 +1,15 @@
+<html> 
+	<head> 
+	<title>New Simple App From Jake</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>Jake Schlottag</h1>
+		<p><b>An Example of PHP in Action</b></p>
+		<?php echo "The Current Date and Time is: <br />"; 
+		echo date("g:i A l, F j Y.");?> </p>
+	<h2>PHP Information</h2> 
+		<p> <?php phpinfo(); ?> </p> 
+	</body> 
+</html>
