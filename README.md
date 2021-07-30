c572a915d3d60a3326de5d77dadbca62b49832c2
Jake Schlottag <jschlottag.1@gmail.com>
2021-07-30 11:08:11 -0400

    revised

---
+++ README.md
@@ -0,0 +1,1 @@
+# My version of simple app
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html> 
 	<head> 
-	<title>New Simple App From Jake</title>
+	<title>PHP Test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
-	<h1>Jake Schlottag</h1>
+	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
