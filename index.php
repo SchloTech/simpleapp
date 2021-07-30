728d174d266d0069adbd0633cbb02bf60b3b9fc0
Jake Schlottag <jschlottag.1@gmail.com>
2021-07-30 11:47:43 -0400

    update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+		<p><b>An Another Example of PHP in Action from Jake</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
